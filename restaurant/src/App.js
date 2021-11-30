5a66006dd34450b26e7290d8551618503e46547f
Ionu <itsbratu@outlook.com>
2021-11-30 17:24:50 +0200

    scroll on top cart

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home/Home.jsx'
 import Login from './Login/Login.jsx'
 import Register from './Register/Register.js';
 import Cart from 'Cart/Cart.jsx';
+import ScrollToTop from 'Utils/scrollToTop.jsx';
 import './index.css'
 
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
@@ -11,13 +12,15 @@ import { CookiesProvider } from 'react-cookie';
 function App() {
   return (
     <CookiesProvider>
-        <Router>
-          <Switch>
-            <Route exact from = "/" to = "/home" component={Home}/>
-            <Route path = "/login" component={Login}/>
-            <Route path = "/register" component={Register}/>
-            <Route path = "/cart" component={Cart}/>
-          </Switch>
+        <Router>         
+            <Switch>
+              <Route exact from = "/" to = "/home" component={Home}/>
+              <Route path = "/login" component={Login}/>
+              <Route path = "/register" component={Register}/>
+              <ScrollToTop>
+                <Route path = "/cart" component={Cart}/>
+              </ScrollToTop>
+            </Switch>
         </Router>
     </CookiesProvider>
   );
